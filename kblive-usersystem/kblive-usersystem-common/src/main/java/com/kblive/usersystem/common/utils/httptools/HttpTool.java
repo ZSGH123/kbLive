1c348f19cc51879df89924e927ce87e778c7509c
zkong <2671242147@qq.com>
2019-07-06 00:12:53 +0800

    测试

---
+++ kblive-usersystem/kblive-usersystem-common/src/main/java/com/kblive/usersystem/common/utils/httptools/HttpTool.java
@@ -0,0 +1,16 @@
+package com.kblive.usersystem.common.utils.httptools;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * title: HttpTool
+ * projectName kbLive
+ * description: 调用http请求的工具
+ * author 2671242147@qq.com
+ * date 2019-07-04 23:50
+ ***/
+public class HttpTool {
+
+
+}
